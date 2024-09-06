b2bfe4def5f8a6646ad65649c696c5ee23a5be62
Kyra4code <lucarioecharmito@gmail.com>
2024-09-06 19:24:47 -0300

    bruh

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import express from "express";
 
 const server = express();
 
-server.listen(3333, ()=>{
+server.listen(3000, ()=>{
     console.log("servidor ligado ☢☢☢")
 })
 
-server.get("/usuario", (request, response)=>{
+server.get("/", (request, response)=>{
     return response.status(200).send({"message": "Meu primeiro endpoint."})
 })
