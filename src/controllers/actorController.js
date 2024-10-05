aec6045ce554dfcb211ba69368c06a2b889eb930
Kyra4code <lucarioecharmito@gmail.com>
2024-10-04 22:35:32 -0300

    ator-finalizado

--- src/controllers/actorController.js
+++ src/controllers/actorController.js
@@ -1,6 +1,6 @@
 import express, { request, response } from 'express';
 import cadastro from '../services/actorService.js';
-
+import atualizacaca0 from '../services/actorService.js'
 
 const route = express.Router();
 
@@ -8,5 +8,13 @@ route.post('/', async(request, response)=>{
     const {nome, sexo, dtn} = request.body;
 
     await cadastro.insertAtor(nome, sexo, dtn);
-    return response.status(201).send({"message": "Ator cadastrado com sucesso!"})
+    return response.status(201).send({"message": "Ator cadastrado com sucesso!"});
+})
+
+route.put("/:idActor", async(request, response)=>{
+    const {idActor} = request.params;
+    const {nome, dtn, sex} = request.body;
+
+    await atualizacaca0.updateAtor(idActor, nome, sex, dtn);
+    return response.status(200).send({"message": "usuario atualizado com sucesso!"});
 })
--- src/services/actorService.js
+++ src/services/actorService.js
@@ -1,12 +1,21 @@
 import database from '../repository/mysql.js';
 
 async function insertAtor(name, sex, dtn){
-    const sql = "insert into tbl_ator(nome_ator, sexo, dt_nascimento) values (?,?,?)"
-    const info = [name, sex, dtn]
+    const sql = "insert into tbl_ator(nome_ator, sexo, dt_nascimento) values (?,?,?)";
+    const info = [name, sex, dtn];
 
     const connection = database.connectDB();
     await connection.query(sql, info);
     connection.end();
 }
 
-export default {insertAtor}
+async function updateAtor(idActor, name, sex, dtn) {
+    const sql = "update tbl_ator set nome_ator = ?, sexo = ?, dt_nascimento = ? where id_ator = ?";
+    const info = [name, sex, dtn, idActor];
+
+    const connection = database.connectDB();
+    await connection.query(sql, info);
+    await connection.end();
+}
+
+export default {insertAtor, updateAtor}
